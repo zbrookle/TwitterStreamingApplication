06a5013b2d461ebec9d8cf1a2ea4004e352d17e4
zbrookle <zachb1996@yahoo.com>
2019-08-03 22:20:52 -0400

    Application now writes live stream directly to csv

--- GUI/src/main/java/com/ZachApp/app/App.java
+++ GUI/src/main/java/com/ZachApp/app/App.java
@@ -3,33 +3,45 @@ package com.ZachApp.app;
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 import java.util.*;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.*;
 
 class TwitterDataStream {
   TwitterStream twitterStream;
   StatusListener listener;
+  BufferedWriter writer;
+  CSVPrinter csvPrinter;
 
   TwitterDataStream() {
     // Initialize the stream instance
     twitterStream = new TwitterStreamFactory().getInstance();
 
+    // Create the csv file to write to
+    try {
+      writer = Files.newBufferedWriter(Paths.get("data.csv"));
+      csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT
+                      .withHeader("UserID",
+                                  "created_at",
+                                  "TextRangeStart",
+                                  "TextRangeEnd",
+                                  "FavoriteCount",
+                                  "Language",
+                                  "Place",
+                                  "RetweetCount",
+                                  "Text",
+                                  "WithheldInCountries",
+                                  "isRetweet"));
+    } catch (IOException e) {
+      System.out.println("ERROR: Problem creating data file");
+    }
+
     // Set up the listener
-    listener = new StatusListener () {
+    listener = new StatusListener() {
       // Create place to store data
-      ArrayList<ArrayList> tweetData = new ArrayList<ArrayList>();
-      String[] HEADERS = {"UserID",
-                          "created_at",
-                          "TextRangeStart",
-                          "TextRangeEnd",
-                          "FavoriteCount",
-                          "Language",
-                          "Place",
-                          "RetweetCount",
-                          "Text",
-                          "WithheldInCountries",
-                          "isRetweet"};
-
       public void onStatus(final Status status) {
-
           // Arrange data in a list
           ArrayList row = new ArrayList() {{
                                   add(status.getUser().getId());
@@ -45,11 +57,18 @@ class TwitterDataStream {
                                   add(status.getWithheldInCountries());
                                   add(status.isRetweet());
                                 }};
+                                
           // Write data out to a CSV file with the appropriate headers
-
-          tweetData.add(row);
-          System.out.println(row);
+          try {
+            csvPrinter.printRecord(row);//status.getUser().getId(), status.getCreatedAt());
+            csvPrinter.flush();
+          } catch (IOException e) {
+            System.out.println("ERROR: Could not write to file");
+            System.out.println(row);
+          }
       }
+
+      // All these methods needed to be overridden according to the api
       public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
       public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
       public void onException(Exception ex) {
@@ -64,9 +83,13 @@ class TwitterDataStream {
   }
 
   public void StreamData(String[] keywords) {
+    // Create a new filter instance
     FilterQuery tweetFilterQuery = new FilterQuery();
+
+    // Add the keywords to the query
     tweetFilterQuery.track(keywords);
 
+    // Start the stream
     twitterStream.filter(tweetFilterQuery);
   }
 }
