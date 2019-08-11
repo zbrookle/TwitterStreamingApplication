886744ab62a861db50ff1aa98b0459b5663ecbdd
zbrookle <zachb1996@yahoo.com>
2019-08-11 00:46:22 -0400

    Fixed style

--- GUI/src/main/java/com/ZachApp/app/App.java
+++ GUI/src/main/java/com/ZachApp/app/App.java
@@ -1,78 +1,116 @@
 package com.ZachApp.app;
 
-// JavaSwing packages
+// JavaFX packages
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.stage.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.Label;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
 import javafx.scene.layout.GridPane;
 import javafx.geometry.Rectangle2D;
 import javafx.geometry.Orientation;
-import javafx.scene.chart.*;
+import javafx.scene.chart.PieChart;
 import javafx.scene.text.Text;
 import java.util.concurrent.atomic.AtomicReference;
 
 // Twitter packages
-import twitter4j.*;
-import twitter4j.conf.ConfigurationBuilder;
-import twitter4j.GeoLocation.*;
+import twitter4j.StatusListener;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.FilterQuery;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StallWarning;
 
 // Java packages
-import java.util.*;
+import java.util.ArrayList;
 
 // CSV Output
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.IOException;
 
 // Spark
-import org.apache.spark.*;
-import org.apache.spark.sql.*;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.SparkSession.Builder;
-import org.apache.spark.sql.streaming.DataStreamReader;
-import org.apache.spark.sql.streaming.*;
-import org.apache.spark.sql.types.*;
-import org.apache.spark.sql.catalog.Column;
+import org.apache.spark.sql.streaming.StreamingQueryException;
+import org.apache.spark.sql.streaming.StreamingQuery;
+import org.apache.spark.sql.types.StructType;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
 
+/**
+ * TwitterDataStream connects to the twitter api and writes tweet data to csv
+ * files and to the UI.
+ */
 class TwitterDataStream {
-  TwitterStream twitterStream;
-  StatusListener listener;
-  BufferedWriter writer;
-  CSVPrinter csvPrinter;
-  int i;
-  int tweetCount;
+  /**
+   * The twitter stream instance.
+   */
+  private TwitterStream twitterStream;
 
+  /**
+    * Gets passed to the csv printer to output data to files.
+    */
+  private BufferedWriter writer;
+
+  /**
+    * Responsible for properly outputting data to csv files.
+    */
+  private CSVPrinter csvPrinter;
+
+  /**
+    * Tracks the partition number.
+    */
+  private int partition;
+
+  /**
+    * Tracks the number of tweets that have been streamed.
+    */
+  private int tweetCount;
+
+  /**
+    * Constructor for the datastream class.
+    * @param twitterFeedVisual is the GUI visual for the twitter feed
+    */
   TwitterDataStream(final GridPane twitterFeedVisual) {
     // Initialize the stream instance
     twitterStream = new TwitterStreamFactory().getInstance();
-    i = 0;
+    partition = 0;
     tweetCount = 0;
 
     // Set up the listener
-    listener = new StatusListener() {
+    StatusListener listener = new StatusListener() {
       // Create place to store data
       public void onStatus(final Status status) {
           // Arrange data in a list
           ArrayList row;
           try {
             row = new ArrayList() {{
-                                    add(status.getUser().getName());
-                                    add(status.getUser().getId());
-                                    add(status.getCreatedAt());
-                                    add(status.getDisplayTextRangeStart());
-                                    add(status.getDisplayTextRangeEnd());
-                                    add(status.getFavoriteCount());
-                                    add(status.getLang());
-                                    add(status.getPlace().getCountry());
-                                    add(status.getPlace().getGeometryCoordinates().toString());
-                                    add(status.getRetweetCount());
-                                    add(status.getText());
-                                    add(status.isRetweet());
-                                  }};
+                    add(status.getUser().getName());
+                    add(status.getUser().getId());
+                    add(status.getCreatedAt());
+                    add(status.getDisplayTextRangeStart());
+                    add(status.getDisplayTextRangeEnd());
+                    add(status.getFavoriteCount());
+                    add(status.getLang());
+                    add(status.getPlace().getCountry());
+                    add(status.getPlace().getGeometryCoordinates().toString());
+                    add(status.getRetweetCount());
+                    add(status.getText());
+                    add(status.isRetweet());
+                  }};
           } catch (NullPointerException e) {
             row = new ArrayList() {{
                                     add(status.getUser().getName());
@@ -86,18 +124,17 @@ class TwitterDataStream {
                                     add("");
                                     add(status.getRetweetCount());
                                     add(status.getText());
-                                    //add(status.getWithheldInCountries());
                                     add(status.isRetweet());
                                   }};
           }
 
           // Add the tweet to the interface
-          Text tweet_text = new Text(status.getText());
-          twitterFeedVisual.addRow(tweetCount, tweet_text);
+          Text tweetText = new Text(status.getText());
+          twitterFeedVisual.addRow(tweetCount, tweetText);
 
           // Create the csv file to write to
           try {
-            writer = Files.newBufferedWriter(Paths.get("stream/data-" + Integer.toString(i) + ".csv"));
+            writer = Files.newBufferedWriter(Paths.get("stream/data-" + Integer.toString(partition) + ".csv"));
             csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT
                             .withHeader("UserName",
                                         "UserID",
@@ -111,14 +148,14 @@ class TwitterDataStream {
                                         "RetweetCount",
                                         "Text",
                                         "isRetweet"));
-            i++;
+            partition++;
           } catch (IOException e) {
             System.out.println("ERROR: Problem creating data file");
           }
 
           // Write data out to a CSV file with the appropriate headers
           try {
-            csvPrinter.printRecord(row);//status.getUser().getId(), status.getCreatedAt());
+            csvPrinter.printRecord(row);
             csvPrinter.flush();
           } catch (IOException e) {
             System.out.println("ERROR: Could not write to file");
@@ -127,20 +164,24 @@ class TwitterDataStream {
       }
 
       // All these methods needed to be overridden according to the api
-      public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
-      public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
-      public void onException(Exception ex) {
+      public void onDeletionNotice(final StatusDeletionNotice statusDeletionNotice) { }
+      public void onTrackLimitationNotice(final int numberOfLimitedStatuses) { }
+      public void onException(final Exception ex) {
           ex.printStackTrace();
       }
-      public void onStallWarning(StallWarning arg0) {}
-      public void onScrubGeo(long arg0,long arg1) {}
+      public void onStallWarning(final StallWarning arg0) { }
+      public void onScrubGeo(final long arg0, final long arg1) { }
     };
 
     // Add the listener
     twitterStream.addListener(listener);
   }
 
-  public void StreamData(String[] keywords) {
+  /**
+    * Method that begins the streaming process.
+    * @param keywords is an array of key words to look for in tweets
+    */
+  public void streamData(final String[] keywords) {
     // Create a new filter instance
     FilterQuery tweetFilterQuery = new FilterQuery();
 
@@ -152,12 +193,33 @@ class TwitterDataStream {
   }
 }
 
+/**
+ * The SparkStreamer handles all aggregations and processing of the twitter
+ * data. It reads from the csv's output by the TwitterDataStream.
+ */
 class SparkStreamer {
+
+  /**
+   * Instance of the spakr session.
+   */
   private SparkSession spark;
+
+  /**
+   * Defines the structure of the data that will be processed by spark.
+   */
   private StructType schema;
+
+  /**
+   * Dataset to store the data output of the twitter stream.
+   */
   private Dataset csvDF;
 
-  SparkStreamer () throws StreamingQueryException {
+  /**
+    * Constructor method for the spark stream.
+    * @throws StreamingQueryException handles errors that may occur during
+    * streaming
+    */
+  SparkStreamer() throws StreamingQueryException {
     // Initialize instance of the spark app
     Builder builder = new Builder();
     spark = builder.master("local").appName("TwitterStream").getOrCreate();
@@ -175,7 +237,6 @@ class SparkStreamer {
       new StructField("Coordinates", DataTypes.StringType, false, Metadata.empty()),
       new StructField("RetweetCount", DataTypes.IntegerType, false, Metadata.empty()),
       new StructField("Text", DataTypes.StringType, false, Metadata.empty()),
-      // new StructField("WithheldInCountries", DataTypes.ArrayType(DataTypes.StringType, false), false, Metadata.empty()),
       new StructField("isRetweet", DataTypes.BooleanType, false, Metadata.empty())
     });
 
@@ -183,7 +244,7 @@ class SparkStreamer {
     csvDF = spark.readStream().option("sep", ",").schema(schema).csv("stream/data*.csv");
     try {
       csvDF.createTempView("TWEET_DATA");
-    } catch(AnalysisException e) {
+    } catch (AnalysisException e) {
       System.out.println("ERROR: Problem creating temp view");
     }
 
@@ -227,21 +288,24 @@ class SparkStreamer {
 //   }
 // }
 
-public class App extends Application{
-  // public static void main(String[] args) {
+/**
+ * App is the user interface and main class. It encompasses user input and
+ * visuals such as graphs. It is implemented using JavaFX.
+ */
+public class App extends Application {
 
-  //
-  //   // Delete files from stream when done
-  //
-  //   // Start Application
-  //   new Hello_World();
-  //
-  // }
+  /**
+    * Variable to store the app's instance of the spark streamer.
+    */
   private SparkStreamer sparkTweets;
+
+  /**
+    * Variable to store the app's twitter feed visual.
+    */
   GridPane twitterFeedPane;
 
   @Override
-  public void start(Stage primaryStage) throws Exception {
+  public void start(final Stage primaryStage) throws Exception {
     // Create a root pane
     GridPane root = new GridPane();
 
@@ -264,10 +328,10 @@ public class App extends Application{
     Button startFeed = new Button("Start Twitter Feed");
     startFeed.setOnAction(new EventHandler<ActionEvent>() {
         @Override
-        public void handle(ActionEvent arg0) {
+        public void handle(final ActionEvent arg0) {
           TwitterDataStream myStream = new TwitterDataStream(twitterFeedPane);
           String[] words = new String[]{keywordsInput.getText()};
-          myStream.StreamData(words);
+          myStream.streamData(words);
           try {
             sparkTweets = new SparkStreamer();
           } catch (StreamingQueryException e) {
@@ -286,8 +350,8 @@ public class App extends Application{
 
     // Twitter feed column
     twitterFeedPane = new GridPane();
-    twitterFeedPane.setMinWidth(screenWidth/3);
-    twitterFeedPane.setMaxWidth(screenWidth/3);
+    twitterFeedPane.setMinWidth(screenWidth / 3);
+    twitterFeedPane.setMaxWidth(screenWidth / 3);
     twitterFeedPane.setMinHeight(screenHeight);
     twitterFeedPane.setMaxHeight(screenHeight);
     ScrollBar feedScrollBar = new ScrollBar();
@@ -313,8 +377,10 @@ public class App extends Application{
     primaryStage.show(); // Show app
   }
 
-  public static void main (String[] args)
-  {
+  /**
+    * Starts the program.
+    */
+  public static void main(final String[] args) {
       launch(args);
   }
 }
